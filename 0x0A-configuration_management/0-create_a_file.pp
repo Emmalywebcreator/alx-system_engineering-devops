64b4e385eec400df13634f3c58b78b804a0af295
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-06-29 01:28:44 -0700

    I love puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# This Puppet code snippet creates a file named /tmp/school
 file { '/tmp/school':
   ensure  => 'present',
   mode    => '0744',
+  owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
 }
