d8fd6124741c4ac8ed7052606feaa2500185dac4
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-06-30 19:44:08 -0700

    Using puppet to config files

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,14 @@
-file { '/etc/ssh/ssh_config':
-  ensure  => present,
-  content => @("EOF")
-    # SSH Client Configuration
-    Host *
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-    | EOF
+#!/usr/bin/env bash
+# Puppet script to create ssh config file
+
+file { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
+file { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
