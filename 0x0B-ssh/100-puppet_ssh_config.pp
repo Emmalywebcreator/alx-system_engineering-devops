daf9f1dd03da5f8fa37aaa49229b54fea8bb777b
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-06-30 19:39:31 -0700

    Using puppet to config files

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,5 @@
 file { '/etc/ssh/ssh_config':
   ensure  => present,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   content => @("EOF")
     # SSH Client Configuration
     Host *
