011e8e913ee0be00dad2fb22c5e3d9abe00b270c
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-07-03 02:06:52 -0700

    puppetlabs-stdlib

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,20 +2,13 @@
 # using puppet to make changes to the default ssh config file
 # so that one can connect to a server without typing a password.
 
-file {  '/etc/ssh/ssh_config':
-  ensure =>  'present',
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+content  => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
 
-file_line { 'Turn off passed auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  match   => '^PasswordAuthentication yes',
-  replace => true,
-}
-
-file_line { 'Declare Identity file':
-  ensure => 'present',
-  line   => 'IdentityFile ~/.ssh.school',
-  match  => '^IdentityFile',
-  path   => '/etc/ssh/ssh_config',
-}
