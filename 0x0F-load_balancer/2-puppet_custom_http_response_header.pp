ac4adf848f98ad35b4c2bf10ac423d5e62753839
Emmanule Ohwoka <emmyprime2015@gmail.com>
2024-07-14 22:13:00 -0700

    shell check

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,7 +4,7 @@
 exec { 'http header':
   command => 'sudo apt-get update -y;
   sudo apt-get install nginx -y;
-  sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME" /etc/nginx/site-enabled/default
+  sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME" /etc/nginx/sites-enabled/default
   sudo nginx service nginx restart',
   provider => shell,
 }
