c818be0ab277f85a264cda16f626a161fbf03e33
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-08-14 01:57:03 +0000

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,8 +17,30 @@ def number_of_subscribers(subreddit):
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"},
     )
+#!/usr/bin/python3
+"""
+number of subscribers for a given subreddit
+"""
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
