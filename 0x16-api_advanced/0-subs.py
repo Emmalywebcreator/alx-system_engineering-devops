162ec328bd3de1ad82041718d84fada1e1a5b8a5
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-08-15 20:13:21 -0700

    resolved indentation error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
+    Function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     """
 
-    if subreddit is None or not isinstance(subreddit, str):
+    if not isinstance(subreddit, str):
         return 0
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
@@ -21,7 +21,7 @@ def number_of_subscribers(subreddit):
     results = response.json()
 
     try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+        return results.get('data', {}).get('subscribers', 0)
+    except (KeyError, TypeError):
         return 0
+
