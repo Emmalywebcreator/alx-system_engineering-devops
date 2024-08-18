fd0328c6368f7990c2e1b27289961377ccf873a9
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2024-08-18 04:08:03 +0000

    Fixing wordpress error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
-#fixing wordpress 501 error by changing phpp to php
-#in /var/www/wp_settings.php
+#fixing wordpress 501 error by changing phpp to php in /var/www/wp_settings.php
 
 exec { 'fix-word_press_server_error':
     command => 'sed -i s/phpp/php/g /var/html/wp-settings',
